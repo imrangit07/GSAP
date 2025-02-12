3e7dbe251b42792a3d4768f6c2ffd1a60e8688ae
imrangit07 <imranhussain21309@gmail.com>
2025-02-12 12:20:09 +0530

    Change box shap

--- moveBox/script.js
+++ moveBox/script.js
@@ -1,9 +1,24 @@
 gsap.to(".box1",{
     x:"1200px",
-    duration:3
+    duration:3,
+    delay:1,
+    rotate:360,
+
+    /*we set the repeat property to -1, so it will be repeated indefinitely.*/
+    
+    repeat:-1, 
+
+    //Let's add a new property : yoyo
+
+    /* Now the animation will go back and forth. Which means that the y position will go from 0 to -20, then to 0 again. And it will be repeated indefinitely.*/
+
+    yoyo:true
 })
 gsap.from(".box2",{
     x:"1200px",
-    duration:3
-
+    duration:3,
+    delay:1,
+    rotate:360,
+    repeat:-1, 
+    yoyo:true
 })
